7bbb746d29717b87069ec5dfdc4c3fead2487afc
Natalja Röhm <roehm.natalia@gmail.com>
2023-08-20 21:42:55 +0200

    HomeWork:change Task 62

--- HomeWorks/src/Task62/Main1.java
+++ HomeWorks/src/Task62/Main1.java
@@ -5,15 +5,6 @@ public class Main1 {
   // Напишите рекурсивный метод public static String reverse(int x),
   // который будет возвращать строку,
   // содержащую десятичные цифры числа в обратном порядке (развернёт число).
-
-  // public static String reverse(int x) {
-  //  if (x >= 0 && x < 10) {
-  //    return Integer.toString(x);
-  //  }
-  //  int digit = x % 10;
-  //  int number = x / 10;
-  //  return Integer.toString(digit) + reverse(number);
-  // }
   public static String reverse(int x) {
     if (x >= 0 && x < 10) {
       return Integer.toString(x);
