df06633c78e2e9184490464043cacf02fe394ae3
Natalja Röhm <roehm.natalia@gmail.com>
2023-08-25 19:36:57 +0200

    Chang:books.add

--- HomeWorks/src/Task64/Main.java
+++ HomeWorks/src/Task64/Main.java
@@ -13,19 +13,19 @@ public class Main {
 
   public static void main(String[] args) {
     List<Book> books = new ArrayList<>();
-    books.add(new Book("Author 1", "Book 1", 100));
-    books.add(new Book("Author 1", "Book 2", 200));
+    books.add(new Book("Author 3", "Book 5", 100));
+    books.add(new Book("Author 1", "Book 3", 200));
     books.add(new Book("Author 1", "Book 3", 150));
-    books.add(new Book("Author 1", "Book 4", 120));
+    books.add(new Book("Author 2", "Book 1", 120));
 
     books.stream()
         .sorted((o1, o2) -> {
           int result = o1.getAuthor().compareTo(o2.getAuthor());
-          if (result == 0) {
-            result = o1.getTitle().compareTo(o2.getTitle());
+          if (result != 0) {
+           return result;
           }
-          return result;
+          return o1.getTitle().compareTo(o2.getTitle());
         })
-        .forEach(book -> System.out.println(book));
+        .forEach(o -> System.out.println(o));
   }
 }
